cc8e1c000aecd7056a251e43a93de2b03278dc5d
Jiyun <jiyunk920@gmail.com>
2025-06-21 12:36:19 +0900

    ♻️  refactor: refactor architecture

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -11,76 +11,100 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
+class App {
+  #map; // private instance variable
+  #mapEvent;
 
-// get two arugments - all are callback functions
-// first one is executed when successfully getting the location
-// second one is executed when failing to get it
-if (navigator.geolocation) {
-  // check if the browser has geolocation API
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      console.log('position:', position);
-      console.log('coords:', position.coords);
-      const { latitude, longitude } = position.coords;
+  constructor() {
+    this._getPosition();
 
-      const coords = [latitude, longitude];
+    // this keyword of the function attacthed to event listener is the element itself
+    // so we need to bind the context of this to the current instance
+    form.addEventListener('submit', this._newWorkout.bind(this));
 
-      // Leftlet gives us L namespace, which gives us functions
-      // L is a global object
-      map = L.map('map').setView(coords, 13);
+    // detect on change of select element
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  _getPosition() {
+    // get two arugments - all are callback functions
+    // first one is executed when successfully getting the location
+    // second one is executed when failing to get it
+    if (navigator.geolocation) {
+      // check if the browser has geolocation API
+      // regular function call, this keyword becomes undefined
+      // so we use bind to set the context of this to the current instance
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Could not get your position!');
+        },
+      );
+    }
+  }
 
-      // map is comming from Leaflet library
-      // on is event handler
-      // Handling clicks on map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus(); // by doing this, user can type the distance field immediately
-      });
-    },
-    function () {
-      alert('Could not get your position!');
-    },
-  );
+  _loadMap(position) {
+    console.log('position:', position);
+    console.log('coords:', position.coords);
+    const { latitude, longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    console.log(this);
+    // Leftlet gives us L namespace, which gives us functions
+    // L is a global object
+    this.#map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // map is comming from Leaflet library
+    // on is event handler
+    // Handling clicks on map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus(); // by doing this, user can type the distance field immediately
+  }
+
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    // display marker
+    console.log(this.#mapEvent);
+    const { lat, lng } = this.#mapEvent.latlng;
+    // add marker on the map
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minwidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        }),
+      )
+      .setPopupContent('workout')
+      .openPopup();
+  }
 }
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  // Clear input fields
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      '';
-
-  // display marker
-  console.log(mapEvent);
-  const { lat, lng } = mapEvent.latlng;
-  // add marker on the map
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minwidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      }),
-    )
-    .setPopupContent('workout')
-    .openPopup();
-});
-
-// detect on change of select element
-inputType.addEventListener('change', function () {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+const app = new App();
